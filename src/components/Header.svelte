9b7b843c18e397ede496760d5c4a553a00e2d4b4
Zngweb <zngweb.com@gmail.com>
2024-01-06 23:01:09 +0530

    update the logo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,14 +1,15 @@
 <script>
     import Nav from './Nav.svelte';
     import MobileMenu from './MobileMenu.svelte';
-    import logo from '$lib/assets/zngweb-logo.png';
+    import logo from '$lib/assets/zngweb-main-logo.png';
+
 </script>
-<header class="max-w-7xl px-6 lg:px-8 mx-auto">
+<header class="max-w-7xl px-6 lg:px-8 mx-auto sm:pl-2">
     <div class="mx-auto max-w-7xl" >
     <div class="flex py-4 items-center justify-between">
     <div>
     <a href="/" class="flex">
-        <img alt="zngweb" src={logo} class="h-14 bg-transparent" />
+        <img alt="zngweb" src={logo} class="h-16 bg-transparent" />
     </a>
   
     </div>
